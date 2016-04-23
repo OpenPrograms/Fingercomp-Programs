f0f18e196dbb23da89a59189b92c46a7d6081b98
Fingercomp <fingercomp@users.noreply.github.com>
2016-04-24 00:42:24 +0600

    Even better delays, and octaves shifting

--- smap/smap/output/pcspkr.output.lua
+++ smap/smap/output/pcspkr.output.lua
@@ -8,6 +8,8 @@ function new()
   return audio.Device(function(dev, chords)
     for _, chord in pairs(chords) do
       for freq, len, instr in pairs(chord) do
+        while freq < 20 do freq = freq * 2 end
+        while freq > 2000 do freq = freq / 2 end
         comp.beep(freq, len / 1000)
       end
     end
