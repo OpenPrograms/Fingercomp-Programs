3af9d675699852335e5bd11024daa4945a675456
Fingercomp <fingercomp@users.noreply.github.com>
2016-08-05 13:41:55 +0700

    Fixed isClosed function

--- libtls/tls.lua
+++ libtls/tls.lua
@@ -1605,7 +1605,7 @@ local function wrapSocket(sock, extensions)
       return sock.id()
     end,
     isClosed = function()
-      return (isClosed and sock.finishConnect()) and true or false
+      return (isClosed and not sock.finishConnect()) and true or false
     end,
     setTimeout = setTimeout
   }
