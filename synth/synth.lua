c49bb590e4809b05efec5050a60dfd8777ecdc77
Fingercomp <fingercomp@users.noreply.github.com>
2017-05-07 15:57:55 +0700

    Implement wave plotter
    The plotter library itself was made by @LeshaInc; I've made some changes to make it work with the buffering library

--- synth/synth.lua
+++ synth/synth.lua
@@ -12,6 +12,8 @@ local sound = com.sound
 
 local w, h = gpu.getViewport()
 
+local sampleRate = 44100
+
 buf.start()
 buf.clear(0x0049C0)
 buf.draw(true)
@@ -92,6 +94,351 @@ local function line(x0, y0, x1, y1)
     end
   end
   return points
+end
+
+local function namedCheckArg(name, value, ...)
+  local t = type(value)
+  local e
+  for _, v in ipairs({...}) do
+    if t == v then
+      return
+    else
+      e = ("bad named argument '%s' (%s expected, got %s)"):format(
+        name, table.concat({...}, " or "), t)
+    end
+  end
+  error(e, 3)
+end
+
+local brailleMap do
+  brailleMap = {}
+  brailleMap.__index = brailleMap
+
+  local function unit(a, b, c, d, e, f, g, h)
+    a = a and 1 or 0
+    b = b and 1 or 0
+    c = c and 1 or 0
+    d = d and 1 or 0
+    e = e and 1 or 0
+    f = f and 1 or 0
+    g = g and 1 or 0
+    h = h and 1 or 0
+    return unicode.char(
+      10240 + 128 * h + 64 * d + 32 * g + 16 * f + 8 * e + 4 * c + 2 * b + a)
+  end
+
+  function brailleMap:set(x, y, v)
+    v = v and v or 0xFFFFFF
+    x = x - 1
+    y = y - 1
+    if x >= 0 and x < self.width and y >= 0 and y < self.height then
+      self.data[self.width * y + x] = v
+    end
+  end
+
+  function brailleMap:get(x, y)
+    x = x - 1
+    y = y - 1
+    if x >= 0 and x < self.width and y >= 0 and y < self.height then
+      return self.data[self.width * y + x] or nil
+    else
+      return nil
+    end
+  end
+
+  local function rgb2hex(r, g, b)
+    return bit32.rshift(r, 16) + bit32.rshift(g, 8) + b
+  end
+
+  local function hex2rgb(hex)
+    return bit32.lshift(hex, 16),
+      bit32.band(bit32.lshift(hex, 8), 0xFF),
+      bit32.band(hex, 0xFF)
+  end
+
+  local function sum(a)
+    local result = 0
+    for _, v in pairs(a) do
+      result = result + v
+    end
+    return result
+  end
+
+  local function color8interpolate(a, b, c, d, e, f, g, h)
+    local count = 0
+    local unique = {}
+    local rarr, garr, barr = {}, {}, {}
+
+    for _, v in ipairs({a or 0, b or 0, c or 0, d or 0, e or 0, f or 0, g or 0, h or 0}) do
+      if v ~= 0 and not unique[v] then
+        unique[v] = true
+        rarr[count + 1], garr[count + 1], barr[count + 1] = hex2rgb(v)
+        count = count + 1
+      end
+    end
+
+    local r = sum(rarr) / count
+    local g = sum(garr) / count
+    local b = sum(barr) / count
+
+    return rgb2hex(r, g, b)
+  end
+
+  function brailleMap:draw(x, y, bg)
+    local sy = 0
+    for dy = 1, self.height, 4 do
+      local sx = 0
+      for dx = 1, self.width, 2 do
+        local a, b, c, d, e, f, g, h =
+          self:get(dx, dy), self:get(dx, dy + 1),
+          self:get(dx, dy + 2), self:get(dx, dy + 3),
+          self:get(dx + 1, dy), self:get(dx + 1, dy + 1),
+          self:get(dx + 1, dy + 2), self:get(dx + 1, dy + 3)
+
+        local nfg = a or b or c or d or e or f or g or h or 0xE1E1E1
+
+        if (a and a ~= nfg) or
+           (b and b ~= nfg) or
+           (c and c ~= nfg) or
+           (d and d ~= nfg) or
+           (e and e ~= nfg) or
+           (f and f ~= nfg) or
+           (g and g ~= nfg) or
+           (h and h ~= nfg) then
+          nfg = color8interpolate(a, b, c, d, e, f, g, h)
+        end
+
+        local c = unit(a, b, c, d, e, f, g, h)
+        if c ~= "⠀" then
+          buf.set(x + sx, y + sy, bg, nfg, c)
+        end
+        sx = sx + 1
+      end
+      sy = sy + 1
+    end
+  end
+
+  setmetatable(brailleMap, {
+    __call = function(_, w, h)
+      local self = setmetatable({}, brailleMap)
+      self.width = w
+      self.height = h
+      self.data = {}
+      return self
+    end
+  })
+end
+
+local plot do
+  plot = {}
+  plot.__index = plot
+
+  function plot:calculateCenter(vx, vy, vw, vh)
+    self.centerY = vy + (vh * math.abs(self.ly)) / (math.abs(self.ly) + self.uy) - 1
+    self.centerX = vx + (vw * math.abs(self.lx)) / (math.abs(self.lx) + self.ux) - 1
+  end
+
+  function plot:drawXAxis(vx, vy, vw, vh)
+    self:calculateCenter(vx, vy, vw, vh)
+    buf.square(vx, math.floor(self.centerY + 0.5), vw - 1, 1, self.background, self.axisColor, "─")
+    buf.set(vx + vw - 1, math.floor(self.centerY + 0.5), self.background, self.axisColor, "→")
+  end
+
+  function plot:drawYAxis(vx, vy, vw, vh)
+    self:calculateCenter(vx, vy, vw, vh)
+    buf.square(math.floor(self.centerX + 0.5), vy + 1, 1, vh - 1, self.background, self.axisColor, "│")
+    buf.set(math.floor(self.centerX + 0.5), vy, self.background, self.axisColor, "↑")
+  end
+
+  function plot:drawXYPoint(vx, vy, vw, vh)
+    buf.set(math.floor(self.centerX + 0.5), math.floor(self.centerY + 0.5), self.background, self.axisColor, "┼")
+  end
+
+  function plot:draw(vx, vy, vw, vh)
+    checkArg(1, vx, "number")
+    checkArg(2, vx, "number")
+    checkArg(3, vx, "number")
+    checkArg(4, vx, "number")
+
+    self.lx, self.ux = self.xRange[1], self.xRange[2]
+    self.ly, self.uy = self.yRange[1], self.yRange[2]
+
+    buf.square(vx, vy, vw, vh, self.background, self.axisColor, " ")
+
+    vh = vh - self:renderLabels(vx, vy, vw, vh)
+    self:calculateCenter(vx, vy, vw, vh)
+
+    if self.isAxisVisible and self.axisPosition == "bottom" then
+      self:drawXAxis(vx, vy, vw, vh)
+      self:drawYAxis(vx, vy, vw, vh)
+      self:drawXYPoint()
+    end
+
+    self.braille = brailleMap(vw * 2, vh * 4)
+
+    for _, fun in pairs(self.functions) do
+      if not fun.hidden then
+        self:plotFunction(vx, vy, vw, vh, fun.fun, fun.color, fun.step)
+      end
+    end
+
+    self.braille:draw(vx, vy, self.background)
+    if self.isAxisVisible and self.axisPosition == "top" then
+      self:drawXAxis(vx, vy, vw, vh)
+      self:drawYAxis(vx, vy, vw, vh)
+      self:drawXYPoint()
+    end
+  end
+
+  function plot:fun(fun, label, color, step)
+    checkArg(1, fun, "function")
+    checkArg(2, label, "string", "nil")
+    checkArg(3, color, "number", "nil")
+    checkArg(4, step, "number", "nil")
+
+    local id = #self.functions + 1
+
+    self.functions[id] = {
+      fun = fun,
+      color = color or 0xe1e1e1,
+      step = step or 0.01,
+      label = label,
+      hidden = false,
+
+      remove = function()
+        self.functions[id] = nil
+      end,
+
+      hide = function()
+        self.functions[id].hidden = true
+      end,
+
+      show = function()
+        self.functions[id].hidden = false
+      end,
+    }
+
+    return self.functions[id]
+  end
+
+  function plot:renderLabels(vx, vy, vw, vh)
+    local sx = 0
+
+    for _, fun in pairs(self.functions) do
+      if not fun.hidden and fun.label then
+        buf.square(vx + sx, vy + vh - 1, unicode.len(fun.label), 1, self.background, fun.color, " ")
+        buf.text(vx + sx, vy + vh - 1, fun.color, fun.label)
+        sx = sx + unicode.len(fun.label) + 2
+      end
+    end
+
+    local keys = {}
+    local n = 0
+
+    for k in pairs(self.labels) do
+      n = n + 1
+      keys[n] = k
+    end
+
+    table.sort(keys)
+
+    for _, k in ipairs(keys) do
+      local label = self.labels[k]
+      if not label.hidden and #label.text ~= 0 then
+        buf.square(vx + sx, vy + vh - 1, unicode.len(label.text), 1, self.background, label.color, " ")
+        buf.text(vx + sx, vy + vh - 1, label.color, label.text)
+        sx = sx + unicode.len(label.text) + 2
+      end
+    end
+
+    return sx > 0 and 1 or 0
+  end
+
+  function plot:plotFunction(vx, vy, vw, vh, fun, color, step)
+    self:calculateCenter(1, 1, vw, vh)
+    local rw, rh = self.ux - self.lx,
+      self.uy - self.ly
+
+    local mx, my, kx, ky = self.centerX * 2,
+      self.centerY * 4, vw * 2 / rw, vh * 4 / rh
+
+    for x = self.lx, self.ux, step do
+      local y = fun(x)
+      local px = kx * x + mx
+      local py = ky * -y + my
+      self.braille:set(math.floor(px + 0.5), math.floor(py + 0.5), color)
+    end
+  end
+
+  function plot:label(text, color)
+    local id = #self.labels + 1
+
+    self.labels[id] = {
+      text = text,
+      color = color or 0xe1e1e1,
+      hidden = false,
+
+      remove = function()
+        self.labels[id] = nil
+      end,
+
+      hide = function()
+        self.labels[id].hidden = true
+      end,
+
+      show = function()
+        self.labels[id].hidden = false
+      end
+    }
+
+    return self.labels[id]
+  end
+
+  setmetatable(plot, {
+    __call = function(_, args)
+      local self = setmetatable({}, plot)
+
+      checkArg(1, args, "table", "nil")
+      args = args or {}
+
+      namedCheckArg("xRange", args.xRange, "table", "nil")
+      namedCheckArg("yRange", args.yRange, "table", "nil")
+
+      self.functions = {}
+      self.labels = {}
+
+      self.xRange = args.xRange or {-1, 1}
+      self.yRange = args.yRange or {-1, 1}
+
+      namedCheckArg("xRange[1]", self.xRange[1], "number")
+      namedCheckArg("xRange[2]", self.xRange[2], "number")
+      namedCheckArg("yRange[1]", self.yRange[1], "number")
+      namedCheckArg("yRange[2]", self.yRange[2], "number")
+
+      assert(self.xRange[1] < self.xRange[2],
+        "axis x: the lower bound must be less than the upper")
+      assert(self.yRange[1] < self.yRange[2],
+        "axis y: the lower bound must be less than the upper")
+
+      namedCheckArg("background", args.background, "number", "nil")
+      self.background = args.background or 0x0f0f0f
+
+      namedCheckArg("axisColor", args.axisColor, "number", "nil")
+      self.axisColor = args.axisColor or 0x1e1e1e
+
+      namedCheckArg("isAxisVisible", args.isAxisVisible, "boolean", "nil")
+      self.isAxisVisible = type(args.isAxisVisible) == "nil" and true
+        or args.isAxisVisible
+
+      namedCheckArg("axisPosition", args.axisPosition, "string", "nil")
+      if args.axisPosition then
+        assert(args.axisPosition == "top" or args.axisPosition == "bottom")
+      end
+      self.axisPosition = args.axisPosition or "bottom"
+
+      return self
+    end
+  })
 end
 
 local function getPinLineCoords(pin)
@@ -446,7 +793,7 @@ function cardVolume(x, y)
     container:addLabel(1, 1, container.width, 1, 0xFFFFFF, "Volume (in percents)")
     local textVolume = container:addInputTextBox(1, 2, container.width, 3, 0xC3C3C3, 0x3C3C3C, 0xFFFFFF, 0x000000, tostring(self._volume * 100))
     function textVolume.validator(text)
-      if text:match("^%d+%.?%d+$") and tonumber(text) <= 100 then
+      if text:match("^%d+%.?%d*$") and tonumber(text) <= 100 then
         return true
       end
       return false
@@ -473,7 +820,7 @@ function cardFrequency(x, y)
     container:addLabel(1, 1, container.width, 1, 0xFFFFFF, "Frequency")
     local textFreq = container:addInputTextBox(1, 2, container.width, 3, 0xC3C3C3, 0x3C3C3C, 0xFFFFFF, 0x000000, tostring(card._frequency))
     function textFreq.validator(text)
-      if text:match("^%d+%.?%d+$") and tonumber(text) < 44100 then
+      if text:match("^%d+%.?%d*$") and tonumber(text) < 44100 then
         return true
       end
       return false
@@ -507,7 +854,7 @@ function cardFM(x, y)
     local label = container:addLabel(1, 1, container.width, 1, 0xFFFFFF, "Modulator intensity")
     local textIntensity = container:addInputTextBox(1, 2, container.width, 3, 0xC3C3C3, 0x3C3C3C, 0xFFFFFF, 0x000000, tostring(self._intensity), "Modulator intensity...")
     function textIntensity.validate(text)
-      if text:match("^%d+%.?%d+$") and tonumber(text) <= 44100 then
+      if text:match("^%d+%.?%d*$") and tonumber(text) <= 44100 then
         return true
       end
       return false
@@ -552,12 +899,179 @@ function cardSoundCard(x, y)
   return card
 end
 
+local generators
+generators = {
+  noise = {
+    generate = function(offset)
+      return math.random(-1000000, 1000000) / 1000000
+    end
+  },
+  square = {
+    generate = function(offset)
+      local v = generators.sine.generate(offset)
+      if v > 0 then
+        v = 1
+      elseif v < 0 then
+        v = -1
+      end
+      return v / 2
+    end
+  },
+  sine = {
+    generate = function(offset)
+      return math.sin(2 * math.pi * offset)
+    end
+  },
+  triangle = {
+    generate = function(offset)
+      return 1 - math.abs(offset - 0.5) * 4
+    end
+  },
+  sawtooth = {
+    generate = function(offset)
+      return 2 * offset - 1
+    end
+  }
+}
+
+local function modulateFrequency(channels, chan, modulator, value)
+  local modChan = channels[modulator.chan]
+  if not modChan then
+    return value
+  end
+  local deviation = modChan:getValue(channels, true) * modulator.index
+  chan.offset = chan.offset + (chan.freq + deviation) / sampleRate
+  return value
+end
+
+local function modulateAmplitude(channels, chan, modulator, value)
+  local modChan = channels[modulator.chan]
+  if not modChan then
+    return value
+  end
+  return value * (1 + modChan:getValue(channels, true))
+end
+
+local function loadChannelConfiguration(chan)
+  local generator = generators.sine
+  local index = chan._chan
+  local freq = 440
+  local offset = 0
+  local freqMod
+  local ampMod
+  local adsr
+  local volume = 1
+  local isFreqMod = false
+  local isAmpMod = false
+  for _, pin in pairs(chan.inputs) do
+    if pin.connected[1] then
+      if pin.type == "wave" then
+        generator = generators[pin.connected[1].card._wave:lower()]
+      elseif pin.type == "freq" then
+        freq = pin.connected[1].card._frequency
+      elseif pin.type == "freqmod" then
+        if pin.connected[1].card.inputs[1].connected[1] then
+          freqMod = {
+            chan = pin.connected[1].card.inputs[1].connected[1].card._chan,
+            index = pin.connected[1].card._intensity
+          }
+        end
+      elseif pin.type == "chan" then
+        ampMod = pin.connected[1].card._chan
+      elseif pin.type == "adsr" then
+        local card = pin.connected[1].card
+        adsr = {
+          attack = card._attack,
+          decay = card._decay,
+          sustain = card._sustain,
+          release = card._release
+        }
+      elseif pin.type == "volume" then
+        volume = pin.connected[1].card._volume
+      end
+    end
+  end
+  for _, pin in pairs(chan.outputs[1].connected) do
+    if pin.card.type == "fm" then  -- FM
+      isFreqMod = true
+    elseif pin.card.type == "chan" then  -- AM
+      isAmpMod = true
+    end
+  end
+  return {
+    generator = generator,
+    index = index,
+    freq = freq,
+    offset = offset,
+    freqMod = freqMod,
+    ampMod = ampMod,
+    adsr = adsr,
+    volume = volume,
+    isFreqMod = isFreqMod,
+    isAmpMod = isAmpMod,
+    getValue = function(self, channels, isModulating)
+      if not isModulating and (self.isFreqMod or self.isAmpMod) then
+        return 0
+      end
+      local value = self.generator.generate(self.offset)
+      if self.freqMod and not self.isFreqMod and not self.isAmpMod then
+        value = modulateFrequency(channels, self, self.freqMod, value)
+      else
+        self.offset = self.offset + self.freq / sampleRate
+      end
+      if self.offset > 1 then
+        self.offset = self.offset % 1
+      end
+      if self.ampMod and not self.ampMod and not self.freqMod then
+        value = modulateAmplitude(channels, self, self.ampMod, value)
+      end
+      if self.adsr then
+        value = value * self.adsr.sustain
+      end
+      return value * self.volume
+    end
+  }
+end
+
+local function loadOutputConfiguration(output)
+  local channels = {}
+  for idx, pin in pairs(output.inputs) do
+    if pin.type == "chan" then
+      if pin.connected[1] then
+        local chanConf = loadChannelConfiguration(pin.connected[1].card)
+        channels[chanConf.index] = chanConf
+      end
+    end
+  end
+  return channels
+end
+
 function cardPlot(x, y)
-  local card = addCard(x, y, 33, 11, 0, 0, 33, 11, "plot", function(self)
-    buf.square(self.x, self.y, 33, 11, 0xFFFFFF, 0x000000, " ")
+  local card = addCard(x, y, 33, 12, 0, 0, 33, 12, "plot", function(self)
+    buf.square(self.x, self.y, 33, 12, 0xFFFFFF, 0x000000, " ")
     buf.text(self.x + 1, self.y + 1, 0x000000, "Plot")
-    buf.square(self.x + 1, self.y + 2, 31, 8, 0x1E1E1E, 0xFFFFFF, " ")
+    buf.text(self.x + 6, self.y + 1, 0x696969, ("x%5d"):format(self._zoom))
+    buf.square(self.x + 1, self.y + 2, 31, 9, 0x1E1E1E, 0xFFFFFF, " ")
+    local p = plot {background=0x1E1E1E, axisColor=0x3C3C3C}
+    local colors = {
+      0x0092FF,
+      0xFFB600,
+      0x992480,
+      0x66B600,
+      0x992400,
+      0xFF6DFF,
+      0x33DBC0,
+      0xCCFF80
+    }
+    local configuration = loadOutputConfiguration(self)
+    for _, chan in pairs(configuration) do
+      chan.plotFunction = p:fun(function()
+        return chan:getValue(configuration)
+      end, "Ch" .. chan.index, colors[chan.index], 1 / sampleRate * self._zoom)
+    end
+    p:draw(self.x + 1, self.y + 2, 31, 9)
   end)
+  card._zoom = 25
   addPin(card, 1, 0, 0xFFFFFF, 0x990000, "Ch1", "chan", side.top, true)
   addPin(card, 5, 0, 0xFFFFFF, 0x990000, "Ch2", "chan", side.top, true)
   addPin(card, 9, 0, 0xFFFFFF, 0x990000, "Ch3", "chan", side.top, true)
@@ -571,6 +1085,19 @@ function cardPlot(x, y)
       if pin.connected[1] then
         pin.connected[1].card:update()
       end
+    end
+  end
+  function card:config(container)
+    container:addLabel(1, 1, container.width, 1, 0xFFFFFF, "Zoom level (small values may cause lag)")
+    local textZoom = container:addInputTextBox(1, 2, container.width, 3, 0xC3C3C3, 0x3C3C3C, 0xFFFFFF, 0x000000, tostring(self._zoom))
+    function textZoom.validator(text)
+      if text:match("^%d+$") and tonumber(text) < 44100 then
+        return true
+      end
+      return false
+    end
+    function textZoom.onInputFinished(text)
+      self._zoom = tonumber(text) or self._zoom
     end
   end
   return card
@@ -628,7 +1155,7 @@ function cardADSR(x, y)
     textDecay.validator = textAttack.validator
     textRelease.validator = textAttack.validator
     function textSustain.validator(text)
-      if text:match("^%d+%.?%d+$") and tonumber(text) <= 100 then
+      if text:match("^%d+%.?%d*$") and tonumber(text) <= 100 then
         return true
       end
       return false
